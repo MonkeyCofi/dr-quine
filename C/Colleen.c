08ca5a7d1730f4923c549fbe382db93e00ea79ab
MonkeyCofi <piercediet06@gmail.com>
2026-06-24 21:27:14 +0400

    modified the comments inside Colleen

--- C/Colleen.c
+++ C/Colleen.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// This comment is outside
+// This comment is outside main
 void	function()
 {
 	return ;
@@ -8,8 +8,8 @@ void	function()
 
 int main()
 {
-	// This comment is inside
-	char *s="#include <stdio.h>%c%c// This comment is outside%cvoid	function()%c{%c%creturn ;%c}%c%cint main()%c{%c%c// This comment is inside%c%cchar *s=%c%s%c;%c%cfunction();%c%cprintf(s, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 34, s, 34, 10, 9, 10, 9, 10, 10);%c}%c";
+	// This comment is inside main
+	char *s="#include <stdio.h>%c%c// This comment is outside main%cvoid	function()%c{%c%creturn ;%c}%c%cint main()%c{%c%c// This comment is inside main%c%cchar *s=%c%s%c;%c%cfunction();%c%cprintf(s, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 34, s, 34, 10, 9, 10, 9, 10, 10);%c}%c";
 	function();
 	printf(s, 10, 10, 10, 10, 10, 9, 10, 10, 10, 10, 10, 9, 10, 9, 34, s, 34, 10, 9, 10, 9, 10, 10);
 }
