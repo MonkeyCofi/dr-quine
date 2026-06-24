04ad59659136653654eb684356fee7ce61a614ac
MonkeyCofi <piercediet06@gmail.com>
2026-06-24 17:50:04 +0400

    made changes to Colleen Assembly

--- Assembly/Colleen.s
+++ Assembly/Colleen.s
@@ -1,99 +1,13 @@
+default rel
 extern printf
+
 section .data
-	fmt db "extern printf%csection .data%c%cfmt db %c%s%c,0%csection .text%c%cglobal main%cfunction:%c%cret%cmain:%c%cpush rbp%c%cmov rbp, rsp%c%ccall function%c%cmov rdi, fmt%c%cmov rsi, 10%c%cmov rdx, 10%c%cmov rcx, 9%c%cmov r8, 34%c%cmov r9, fmt%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 9%c%cpush 10%c%cpush 10%c%cpush 34%c%cxor eax, eax%c%ccall printf%c%cadd rsp, 584%c%cleave%c%cret%c",0
-section .text
-	global main
-function:
-	ret
+    msg db "extern printf%c%csection .data%c%cmsg db %s", 0
+    len equ $ - msg
+
+global main
+
 main:
-	push rbp
-	mov rbp, rsp
-	call function
-	mov rdi, fmt
-	mov rsi, 10
-	mov rdx, 10
-	mov rcx, 9
-	mov r8, 34
-	mov r9, fmt
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 9
-	push 10
-	push 10
-	push 34
-	xor eax, eax
-	call printf wrt ..plt
-	add rsp, 584
-	leave
-	ret
+    mov rdi, msg
+    
+    call print WRT ..plt
