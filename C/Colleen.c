a80bcc78b4d8037657482f7e33636a7271b3e413
MonkeyCofi <piercediet06@gmail.com>
2026-06-24 13:41:08 +0400

    finished Colleen maybe. Doesn't reproduce the header section

--- C/Colleen.c
+++ C/Colleen.c
@@ -6,13 +6,13 @@
 /*   By: pipolint <pipolint@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/23 20:25:39 by pipolint          #+#    #+#             */
-/*   Updated: 2026/06/23 21:21:10 by pipolint         ###   ########.fr       */
+/*   Updated: 2026/06/24 13:40:34 by pipolint         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-// This comment is outside
 
+// This comment is outside
 void	function()
 {
 	printf("hello, world\n");
@@ -20,6 +20,7 @@ void	function()
 
 int main()
 {
-	char *s="#include <stdio.h>%c// This comment is outside%cint main()%c{%c%c// This comment is inside%c%cchar *s=%c%s%c;%c%cprintf(s, 10, 10, 10, 10, 9, 10, 9, 34, s, 34, 10, 9, 10, 10);%c}%c";
-	printf(s, 10, 10, 10, 10, 9, 10, 9, 34, s, 34, 10, 9, 10, 10);
+	char *s="#include <stdio.h>%c%c// This comment is outside%cvoid	function()%c{%c%cprintf(%chello, world\\n%c);%c}%c%cint main()%c{%c%c// This comment is inside%c%cchar *s=%c%s%c;%c%cfunction();%c%cprintf(s, 10, 10, 10, 10, 10, 9, 34, 34, 10, 10, 10, 10, 10, 9, 10, 9, 34, s, 34, 10, 9, 10, 9, 10, 10);%c}%c";
+	function();
+	printf(s, 10, 10, 10, 10, 10, 9, 34, 34, 10, 10, 10, 10, 10, 9, 10, 9, 34, s, 34, 10, 9, 10, 9, 10, 10);
 }
