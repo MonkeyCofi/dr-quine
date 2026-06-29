819a9b651d30b0cd48b09e73df9534e123e0eb5d
MonkeyCofi <piercediet06@gmail.com>
2026-06-29 16:26:38 +0400

    grace changed

--- C/Grace.c
+++ C/Grace.c
@@ -1,5 +1,2 @@
-#include<stdio.h>
-#define B(x) int main(void){FILE *fp=fopen("Grace_kid.c", "RW");\n fputs("#define B(x)"x"\n#define X(s) S(s)\n#define S(s) #s\nB(X(B(x)));",fp)}
-#define X(s) S(s)
-#define S(s) #s
-B(X(B(x)));
+#include <stdio.h>
+#define M() int main() {}
