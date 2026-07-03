9b198ddfceb2264e5953a4a5d60bdccb1d9e1757
MonkeyCofi <piercediet06@gmail.com>
2026-07-03 14:52:03 +0400

    finished asm Colleen

--- C/Grace_kid.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-// This is a comment
-#define C "#include <stdio.h>%c// This is a comment%c#define C %c%s%c%c#define MAIN() int main(void) {FILE *fp=fopen(%cGrace_kid.c%c, %cw+%c); fprintf(fp, C,10,10,34,C,34,10,34,34,34,34,10);}%cMAIN()"
-#define MAIN() int main(void) {FILE *fp=fopen("Grace_kid.c", "w+"); fprintf(fp, C,10,10,34,C,34,10,34,34,34,34,10);}
-MAIN()
