fe5b9934f0253a430ba4e400871ff913ec050c22
MonkeyCofi <piercediet06@gmail.com>
2026-07-03 20:58:47 +0400

    more advances made omgah

--- Assembly/Grace.s
+++ Assembly/Grace.s
@@ -1,5 +1,5 @@
 ; Comment
-%define C "; Comment%2$c%%define C %1$c%4$s%1$c%2$c%%define FNAME %1$cGrace_kid.s%1$c%2$c%%macro real_main 0%2$csection .data%2$c%3$csrc db, C, 0%2$c%3$cfname db FNAME, 0%2$c"
+%define C "; Comment%2$c%%define C %1$c%4$s%1$c%2$c%%define FNAME %1$cGrace_kid.s%1$c%2$c%%macro real_main 0%2$csection .data%2$c%3$csrc db, C, 0%2$c%3$cfname db FNAME, 0%2$c%2$csection .text%2$c%3$cglobal main%2$c%3$cextern dprintf%2$c%2$c"
 %define FNAME "Grace_kid.s"
 %macro real_main 0
 section .data
@@ -9,7 +9,6 @@ section .data
 section .text
     global main
     extern dprintf
-    extern printf
 
 main:
     xor rax, rax
--- Assembly/Grace_kid.s
+++ Assembly/Grace_kid.s
@@ -1,7 +1,12 @@
 ; Comment
-%define C "; Comment%2$c%%define C %1$c%4$s%1$c%2$c%%define FNAME %1$cGrace_kid.s%1$c%2$c%%macro real_main 0%2$csection .data%2$c%3$csrc db, C, 0%2$c%3$cfname db FNAME, 0%2$c"
+%define C "; Comment%2$c%%define C %1$c%4$s%1$c%2$c%%define FNAME %1$cGrace_kid.s%1$c%2$c%%macro real_main 0%2$csection .data%2$c%3$csrc db, C, 0%2$c%3$cfname db FNAME, 0%2$c%2$csection .text%2$c%3$cglobal main%2$c%3$cextern dprintf%2$c%2$c"
 %define FNAME "Grace_kid.s"
 %macro real_main 0
 section .data
 	src db, C, 0
 	fname db FNAME, 0
+
+section .text
+	global main
+	extern dprintf
+
