b6cd78bc7ada8b061c341ad1ddd222fb33270992
MonkeyCofi <piercediet06@gmail.com>
2026-07-04 16:46:16 +0400

    finished completely

--- C/Grace.c
+++ C/Grace.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 // This is a comment
 #define OFILE "Grace_kid.c"
-#define C "#include <stdio.h>%c// This is a comment%c#define OFILE %cGrace_kid.c%c%c#define C %c%s%c%c#define MAIN() int main(void) {FILE *fp=fopen(OFILE, %cw+%c); fprintf(fp,C,10,10,34,34,10,34,C,34,10,34,34,10); fclose(fp);}%cMAIN()"
-#define MAIN() int main(void) {FILE *fp=fopen(OFILE, "w+"); fprintf(fp,C,10,10,34,34,10,34,C,34,10,34,34,10); fclose(fp);}
+#define C "#include <stdio.h>%c// This is a comment%c#define OFILE %cGrace_kid.c%c%c#define C %c%s%c%c#define MAIN() int main(void) {FILE *fp=fopen(OFILE, %cw+%c); if (fp == NULL) return 1; fprintf(fp,C,10,10,34,34,10,34,C,34,10,34,34,10); fclose(fp);}%cMAIN()"
+#define MAIN() int main(void) {FILE *fp=fopen(OFILE, "w+"); if (fp == NULL) return 1; fprintf(fp,C,10,10,34,34,10,34,C,34,10,34,34,10); fclose(fp);}
 MAIN()
--- C/Sully.c
+++ C/Sully.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define CODE "#include <stdio.h>%c#include <stdlib.h>%c#define CODE %c%s%c%cint main(void) {%c%cint i = %d;%c%cif (i <= 0) return 0;%c%cchar filename[50];%c%csprintf(filename, %cSully_%%d.c%c, i--);%c%cFILE *fp = fopen(filename, %cw+%c);%c%cfprintf(fp, CODE,10,10,34,CODE,34,10,10,9,i,10,9,10,9,10,9,34,34,10,9,34,34,10,9,10,9,10,9,10,9,34,34,10,9,10,9,34,34,10,9,10,9,34,34,10,9,10);%c%cfclose(fp);%c%cchar exec_file[50];%c%csprintf(exec_file, %cSully_%%d%c, i + 1);%c%cchar cmd[200];%c%csprintf(cmd, %cgcc -Wall -Wextra -Werror %%s -o %%s%c, filename, exec_file);%c%csystem(cmd);%c%csprintf(cmd, %c./%%s%c, exec_file);%c%csystem(cmd);%c}"
+#define CODE "#include <stdio.h>%1$c#include <stdlib.h>%1$c#define CODE %2$c%4$s%2$c%1$cint main(void) {%1$c%3$cint i = %5$d;%1$c%3$cif (i <= 0) return 0;%1$c%3$cchar filename[50];%1$c%3$csprintf(filename, %2$cSully_%%d.c%2$c, i--);%1$c%3$cFILE *fp = fopen(filename, %2$cw+%2$c);%1$c%3$cif (fp == NULL)%1$c%3$c%3$creturn 1;%1$c%3$cfprintf(fp,CODE,10,34,9,CODE,i);%1$c%3$cfclose(fp);%1$c%3$cchar exec_file[50];%1$c%3$csprintf(exec_file, %2$cSully_%%d%2$c, i + 1);%1$c%3$cchar cmd[200];%1$c%3$csprintf(cmd, %2$cgcc -Wall -Wextra -Werror %%s -o %%s%2$c, filename, exec_file);%1$c%3$csystem(cmd);%1$c%3$csprintf(cmd, %2$c./%%s%2$c, exec_file);%1$c%3$csystem(cmd);%1$c}"
 int main(void) {
     int i = 5;
     if (i <= 0) return 0;
     char filename[50];
     sprintf(filename, "Sully_%d.c", i--);
     FILE *fp = fopen(filename, "w+");
-    fprintf(fp, CODE,10,10,34,CODE,34,10,10,9,i,10,9,10,9,10,9,34,34,10,9,34,34,10,9,10,9,10,9,10,9,34,34,10,9,10,9,34,34,10,9,10,9,34,34,10,9,10);
+    if (fp == NULL)
+        return 1;
+    fprintf(fp,CODE,10,34,9,CODE,i);
     fclose(fp);
     char exec_file[50];
     sprintf(exec_file, "Sully_%d", i + 1);
