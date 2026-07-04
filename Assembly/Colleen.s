bc22d538a95519d3b0955a1a71b4aede91873766
MonkeyCofi <piercediet06@gmail.com>
2026-07-04 19:40:39 +0400

    finished

--- Assembly/Colleen.s
+++ Assembly/Colleen.s
@@ -1,4 +1,5 @@
-%define S "%%define S %1$c%2$s%1$c%3$c%3$c; This comment is outside the entrypoint%3$csection .data%3$c%4$cmsg db S, 0%3$c%3$csection .text%3$c%4$cglobal main%3$c%4$cextern printf%3$c%3$croutine:%3$c%4$cpush rbp%3$c%4$ccall printf%3$c%4$cxor rax, rax%3$c%4$cpop rbp%3$c%4$cret%3$c%3$cmain:%3$c%4$c; This comment is inside the entrypoint%3$c%4$cxor rax, rax%3$c%4$cpush rbp%3$c%4$cmov rdi, msg%3$c%4$cmov rsi, 34%3$c%4$cmov rdx, msg%3$c%4$cmov rcx, 10%3$c%4$cmov r8, 9%3$c%4$ccall routine%3$c%4$cpop rbp%3$c%4$cret%3$c"
+default rel
+%define S "default rel%3$c%%define S %1$c%2$s%1$c%3$c%3$c; This comment is outside the entrypoint%3$csection .data%3$c%4$cmsg db S, 0%3$c%3$csection .text%3$c%4$cglobal main%3$c%4$cextern printf%3$c%3$croutine:%3$c%4$cpush rbp%3$c%4$ccall printf%3$c%4$cxor rax, rax%3$c%4$cpop rbp%3$c%4$cret%3$c%3$cmain:%3$c%4$c; This comment is inside the entrypoint%3$c%4$cxor rax, rax%3$c%4$cpush rbp%3$c%4$cmov rdi, msg%3$c%4$cmov rsi, 34%3$c%4$cmov rdx, msg%3$c%4$cmov rcx, 10%3$c%4$cmov r8, 9%3$c%4$ccall routine%3$c%4$cpop rbp%3$c%4$cret%3$c"
 
 ; This comment is outside the entrypoint
 section .data
--- Assembly/Sully.s
+++ Assembly/Sully.s
@@ -1,4 +1,5 @@
-%define C "%%define C %3$c%4$s%3$c%1$c%1$csection .data%1$c%2$csrc db C, 0%1$c%2$csrc_file db %3$cSully_%%d.s%3$c, 0%1$c%2$cfilename db %3$cSully_%%d%3$c, 0%1$c%2$cmode db %3$cw+%3$c, 0%1$c%2$cexec db %3$cnasm -f elf64 Sully_%%1$d.s && gcc -no-pie Sully_%%1$d.o -o Sully_%%1$d && ./Sully_%%d%3$c%1$c%1$csection .bss%1$c%2$cbuffer resb 64%1$c%2$cfile_ptr resq 1%1$c%1$csection .text%1$c%2$cglobal main%1$c%2$cextern sprintf%1$c%2$cextern fprintf%1$c%2$cextern fopen%1$c%2$cextern fclose%1$c%2$cextern system%1$c%2$cextern printf%1$c%1$cmain:%1$c%2$cxor rax, rax%1$c%2$cmov rax, %5$d%1$c%2$ctest rax, rax%1$c%2$cjz .done%1$c%2$cpush rax%1$c%2$clea rdi, [rel buffer]%1$c%2$clea rsi, [rel src_file]%1$c%2$cmov rdx, rax%1$c%2$ccall sprintf%1$c%2$cpop rax%1$c%1$c%2$clea rdi, [rel buffer]%1$c%2$clea rsi, [rel mode]%1$c%2$cpush rax%1$c%2$ccall fopen%1$c%2$ctest rax, rax%1$c%2$cjz .error%1$c%2$cmov [rel file_ptr], rax%1$c%2$cpop rax%1$c%1$c%2$cxor rdi, rdi%1$c%2$cmov rdi, rax%1$c%2$cdec rdi%1$c%2$cpush qword rdi%1$c%2$cmov rdi, [rel file_ptr]%1$c%2$cmov rsi, src%1$c%2$cmov rdx, 10%1$c%2$cmov rcx, 9%1$c%2$cmov r8, 34%1$c%2$cmov r9, src%1$c%2$ccall fprintf%1$c%2$cpop rax%1$c%2$cinc rax%1$c%1$c%2$cpush rax%1$c%2$cmov rdi, [rel file_ptr]%1$c%2$ccall fclose%1$c%2$cpop rax%1$c%1$c%2$clea rdi, [rel buffer]%1$c%2$clea rsi, [rel exec]%1$c%2$cmov rdx, rax%1$c%2$cpush rax%1$c%2$ccall sprintf%1$c%2$cpop rax%1$c%2$clea rdi, [rel buffer]%1$c%2$cpush rax%1$c%2$ccall system%1$c%2$cpop rax%1$c%2$cret%1$c%1$c.error:%1$c%2$cmov rax, 1%1$c%2$cret%1$c%1$c.done:%1$c%2$cmov rax,0%1$c%2$cret"
+default rel
+%define C "default rel%1$c%%define C %3$c%4$s%3$c%1$c%1$csection .data%1$c%2$csrc db C, 0%1$c%2$csrc_file db %3$cSully_%%d.s%3$c, 0%1$c%2$cfilename db %3$cSully_%%d%3$c, 0%1$c%2$cmode db %3$cw+%3$c, 0%1$c%2$cexec db %3$cnasm -f elf64 Sully_%%1$d.s && gcc -no-pie Sully_%%1$d.o -o Sully_%%1$d && ./Sully_%%d%3$c%1$c%1$csection .bss%1$c%2$cbuffer resb 64%1$c%2$cfile_ptr resq 1%1$c%1$csection .text%1$c%2$cglobal main%1$c%2$cextern sprintf%1$c%2$cextern fprintf%1$c%2$cextern fopen%1$c%2$cextern fclose%1$c%2$cextern system%1$c%2$cextern printf%1$c%1$cmain:%1$c%2$cxor rax, rax%1$c%2$cmov rax, %5$d%1$c%2$ctest rax, rax%1$c%2$cjz .done%1$c%2$cpush rax%1$c%2$clea rdi, [buffer]%1$c%2$clea rsi, [src_file]%1$c%2$cmov rdx, rax%1$c%2$ccall sprintf%1$c%2$cpop rax%1$c%1$c%2$clea rdi, [buffer]%1$c%2$clea rsi, [mode]%1$c%2$cpush rax%1$c%2$ccall fopen%1$c%2$ctest rax, rax%1$c%2$cjz .error%1$c%2$cmov [file_ptr], rax%1$c%2$cpop rax%1$c%1$c%2$cxor rdi, rdi%1$c%2$cmov rdi, rax%1$c%2$cdec rdi%1$c%2$cpush qword rdi%1$c%2$cmov rdi, [file_ptr]%1$c%2$cmov rsi, src%1$c%2$cmov rdx, 10%1$c%2$cmov rcx, 9%1$c%2$cmov r8, 34%1$c%2$cmov r9, src%1$c%2$ccall fprintf%1$c%2$cpop rax%1$c%2$cinc rax%1$c%1$c%2$cpush rax%1$c%2$cmov rdi, [file_ptr]%1$c%2$ccall fclose%1$c%2$cpop rax%1$c%1$c%2$clea rdi, [buffer]%1$c%2$clea rsi, [exec]%1$c%2$cmov rdx, rax%1$c%2$cpush rax%1$c%2$ccall sprintf%1$c%2$cpop rax%1$c%2$clea rdi, [buffer]%1$c%2$cpush rax%1$c%2$ccall system%1$c%2$cpop rax%1$c%2$cret%1$c%1$c.error:%1$c%2$cmov rax, 1%1$c%2$cret%1$c%1$c.done:%1$c%2$cmov rax,0%1$c%2$cret"
 
 section .data
     src db C, 0
@@ -26,26 +27,26 @@ main:
     test rax, rax
     jz .done
     push rax
-    lea rdi, [rel buffer]
-    lea rsi, [rel src_file]
+    lea rdi, [buffer]
+    lea rsi, [src_file]
     mov rdx, rax
     call sprintf
     pop rax
 
-    lea rdi, [rel buffer]
-    lea rsi, [rel mode]
+    lea rdi, [buffer]
+    lea rsi, [mode]
     push rax
     call fopen
     test rax, rax
     jz .error
-    mov [rel file_ptr], rax
+    mov [file_ptr], rax
     pop rax
 
     xor rdi, rdi
     mov rdi, rax
     dec rdi
     push qword rdi 
-    mov rdi, [rel file_ptr]
+    mov rdi, [file_ptr]
     mov rsi, src
     mov rdx, 10
     mov rcx, 9
@@ -56,18 +57,18 @@ main:
     inc rax
 
     push rax
-    mov rdi, [rel file_ptr]
+    mov rdi, [file_ptr]
     call fclose
     pop rax
 
-    lea rdi, [rel buffer]
-    lea rsi, [rel exec]
+    lea rdi, [buffer]
+    lea rsi, [exec]
     mov rdx, rax
     push rax
     call sprintf
     pop rax
 
-    lea rdi, [rel buffer]
+    lea rdi, [buffer]
     push rax
     call system
     pop rax
