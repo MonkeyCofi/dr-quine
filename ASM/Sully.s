7f4682c4a3c573b791386cc18451110c335b53d8
MonkeyCofi <piercediet06@gmail.com>
2026-07-05 17:02:34 +0400

    fixed Sully in both ASM and C. changed Assembly folder to ASM

---
+++ ASM/Sully.s
@@ -0,0 +1,83 @@
+default rel
+%define C "default rel%1$c%%define C %3$c%4$s%3$c%1$c%1$csection .data%1$c%2$csrc db C, 0%1$c%2$csrc_file db %3$cSully_%%d.s%3$c, 0%1$c%2$cfilename db %3$cSully_%%d%3$c, 0%1$c%2$cmode db %3$cw+%3$c, 0%1$c%2$cexec db %3$cnasm -f elf64 Sully_%%1$d.s && gcc -no-pie Sully_%%1$d.o -o Sully_%%1$d && ./Sully_%%d%3$c%1$c%1$csection .bss%1$c%2$cbuffer resb 64%1$c%2$cfile_ptr resq 1%1$c%1$csection .text%1$c%2$cglobal main%1$c%2$cextern sprintf%1$c%2$cextern fprintf%1$c%2$cextern fopen%1$c%2$cextern fclose%1$c%2$cextern system%1$c%2$cextern printf%1$c%1$cmain:%1$c%2$cxor rax, rax%1$c%2$cmov rax, %5$d%1$c%2$ctest rax, rax%1$c%2$cjs .done%1$c%2$cpush rax%1$c%2$clea rdi, [buffer]%1$c%2$clea rsi, [src_file]%1$c%2$cmov rdx, rax%1$c%2$ccall sprintf%1$c%2$cpop rax%1$c%1$c%2$clea rdi, [buffer]%1$c%2$clea rsi, [mode]%1$c%2$cpush rax%1$c%2$ccall fopen%1$c%2$ctest rax, rax%1$c%2$cjz .error%1$c%2$cmov [file_ptr], rax%1$c%2$cpop rax%1$c%1$c%2$cxor rdi, rdi%1$c%2$cmov rdi, rax%1$c%2$cdec rdi%1$c%2$cpush qword rdi%1$c%2$cmov rdi, [file_ptr]%1$c%2$cmov rsi, src%1$c%2$cmov rdx, 10%1$c%2$cmov rcx, 9%1$c%2$cmov r8, 34%1$c%2$cmov r9, src%1$c%2$ccall fprintf%1$c%2$cpop rax%1$c%2$cinc rax%1$c%1$c%2$cpush rax%1$c%2$cmov rdi, [file_ptr]%1$c%2$ccall fclose%1$c%2$cpop rax%1$c%1$c%2$clea rdi, [buffer]%1$c%2$clea rsi, [exec]%1$c%2$cmov rdx, rax%1$c%2$cpush rax%1$c%2$ccall sprintf%1$c%2$cpop rax%1$c%1$c%2$clea rdi, [buffer]%1$c%2$cpush rax%1$c%2$ccall system%1$c%2$cpop rax%1$c%2$cret%1$c%1$c.error:%1$c%2$cmov rax, 1%1$c%2$cret%1$c%1$c.done:%1$c%2$cmov rax, 0%1$c%2$cret"
+
+section .data
+	src db C, 0
+	src_file db "Sully_%d.s", 0
+	filename db "Sully_%d", 0
+	mode db "w+", 0
+	exec db "nasm -f elf64 Sully_%1$d.s && gcc -no-pie Sully_%1$d.o -o Sully_%1$d && ./Sully_%d"
+
+section .bss
+	buffer resb 64
+	file_ptr resq 1
+
+section .text
+	global main
+	extern sprintf
+	extern fprintf
+	extern fopen
+	extern fclose
+	extern system
+	extern printf
+
+main:
+	xor rax, rax
+	mov rax, 5
+	test rax, rax
+	js .done
+	push rax
+	lea rdi, [buffer]
+	lea rsi, [src_file]
+	mov rdx, rax
+	call sprintf
+	pop rax
+
+	lea rdi, [buffer]
+	lea rsi, [mode]
+	push rax
+	call fopen
+	test rax, rax
+	jz .error
+	mov [file_ptr], rax
+	pop rax
+
+	xor rdi, rdi
+	mov rdi, rax
+	dec rdi
+	push qword rdi
+	mov rdi, [file_ptr]
+	mov rsi, src
+	mov rdx, 10
+	mov rcx, 9
+	mov r8, 34
+	mov r9, src
+	call fprintf
+	pop rax
+	inc rax
+
+	push rax
+	mov rdi, [file_ptr]
+	call fclose
+	pop rax
+
+	lea rdi, [buffer]
+	lea rsi, [exec]
+	mov rdx, rax
+	push rax
+	call sprintf
+	pop rax
+
+	lea rdi, [buffer]
+	push rax
+	call system
+	pop rax
+	ret
+
+.error:
+	mov rax, 1
+	ret
+
+.done:
+	mov rax, 0
+	ret
--- C/Sully.c
+++ C/Sully.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define CODE "#include <stdio.h>%1$c#include <stdlib.h>%1$c#define CODE %2$c%4$s%2$c%1$cint main(void) {%1$c%3$cint i = %5$d;%1$c%3$cif (i <= 0) return 0;%1$c%3$cchar filename[50];%1$c%3$csprintf(filename, %2$cSully_%%d.c%2$c, i--);%1$c%3$cFILE *fp = fopen(filename, %2$cw+%2$c);%1$c%3$cif (fp == NULL)%1$c%3$c%3$creturn 1;%1$c%3$cfprintf(fp,CODE,10,34,9,CODE,i);%1$c%3$cfclose(fp);%1$c%3$cchar exec_file[50];%1$c%3$csprintf(exec_file, %2$cSully_%%d%2$c, i + 1);%1$c%3$cchar cmd[200];%1$c%3$csprintf(cmd, %2$cgcc -Wall -Wextra -Werror %%s -o %%s%2$c, filename, exec_file);%1$c%3$csystem(cmd);%1$c%3$csprintf(cmd, %2$c./%%s%2$c, exec_file);%1$c%3$csystem(cmd);%1$c}"
+#define CODE "#include <stdio.h>%1$c#include <stdlib.h>%1$c#define CODE %2$c%4$s%2$c%1$cint main(void) {%1$c%3$cint i = %5$d;%1$c%3$cif (i < 0) return 0;%1$c%3$cchar filename[50];%1$c%3$csprintf(filename, %2$cSully_%%d.c%2$c, i--);%1$c%3$cFILE *fp = fopen(filename, %2$cw+%2$c);%1$c%3$cif (fp == NULL)%1$c%3$c%3$creturn 1;%1$c%3$cfprintf(fp,CODE,10,34,9,CODE,i);%1$c%3$cfclose(fp);%1$c%3$cchar exec_file[50];%1$c%3$csprintf(exec_file, %2$cSully_%%d%2$c, i + 1);%1$c%3$cchar cmd[200];%1$c%3$csprintf(cmd, %2$cgcc -Wall -Wextra -Werror %%s -o %%s%2$c, filename, exec_file);%1$c%3$csystem(cmd);%1$c%3$csprintf(cmd, %2$c./%%s%2$c, exec_file);%1$c%3$csystem(cmd);%1$c}"
 int main(void) {
-    int i = 5;
-    if (i <= 0) return 0;
-    char filename[50];
-    sprintf(filename, "Sully_%d.c", i--);
-    FILE *fp = fopen(filename, "w+");
-    if (fp == NULL)
-        return 1;
-    fprintf(fp,CODE,10,34,9,CODE,i);
-    fclose(fp);
-    char exec_file[50];
-    sprintf(exec_file, "Sully_%d", i + 1);
-    char cmd[200];
-    sprintf(cmd, "gcc -Wall -Wextra -Werror %s -o %s", filename, exec_file);
-    system(cmd);
-    sprintf(cmd, "./%s", exec_file);
-    system(cmd);
+	int i = 5;
+	if (i < 0) return 0;
+	char filename[50];
+	sprintf(filename, "Sully_%d.c", i--);
+	FILE *fp = fopen(filename, "w+");
+	if (fp == NULL)
+		return 1;
+	fprintf(fp,CODE,10,34,9,CODE,i);
+	fclose(fp);
+	char exec_file[50];
+	sprintf(exec_file, "Sully_%d", i + 1);
+	char cmd[200];
+	sprintf(cmd, "gcc -Wall -Wextra -Werror %s -o %s", filename, exec_file);
+	system(cmd);
+	sprintf(cmd, "./%s", exec_file);
+	system(cmd);
 }
