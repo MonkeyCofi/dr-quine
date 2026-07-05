185672fa93474251c78c76aea1b39379064b0921
MonkeyCofi <piercediet06@gmail.com>
2026-07-05 23:01:44 +0400

    idk whats going on with java lowkenuinely

--- Java/Grace.java
+++ Java/Grace.java
@@ -1,8 +1,14 @@
-public class Grace {
-    public static void main(String args[]) {
-        public final static String KID = "Grace_kid.java";
-        public final static String SOURCE = "public class Grace{%2$c%"
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
 
-        System.out.printf(SOURCE, 9, 10, 34, SOURCE)
+public class Grace {
+    public final static String KID = "Grace_kid.java";
+    public final static String SOURCE = "import java.io.PrintWriter;%2$cimport java.io.FileNotFoundException;%2$c%2$cpublic class Grace {%2$c%1$cpublic static void main(String args[]) {%2$c%1$c%1$cpublic final static String KID=%3$cGrace_kid.java%3$c;%2$c%1$c%1$cpublic final static String SOURCE = %3$c%4$s%3$c;%2$c}";
+    public static void main(String args[]) {
+        try (PrintWriter w = new PrintWriter(KID)) {
+            w.printf(SOURCE, 9, 10, 34, SOURCE);
+        } catch (FileNotFoundException e) {
+            return ;
+        }
     }
 }
