f1be19d27769ea90839461249f511a1c3f964f48
MonkeyCofi <piercediet06@gmail.com>
2026-07-05 18:59:15 +0400

    created Grace.java

--- Java/Colleen.java
+++ Java/Colleen.java
@@ -3,9 +3,9 @@ public class Colleen {
 		return ;
 	}
 	// Inside
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 		// Outside
-		String src = "public class Colleen {%2$c%1$cprivate static void routine() {%2$c%1$c%1$creturn ;%2$c%1$c}%2$c%1$c// Inside%2$c%1$cpublic static void main(String[] args) {%2$c%1$c%1$c// Outside%2$c%1$c%1$cString src = %3$c%4$s%3$c;%2$c%1$c%1$cSystem.out.printf(src, 9, 10, 34, src);%2$c%1$c%1$croutine();%2$c%1$c}%2$c}%2$c";
+		String src = "public class Colleen {%2$c%1$cprivate static void routine() {%2$c%1$c%1$creturn ;%2$c%1$c}%2$c%1$c// Inside%2$c%1$cpublic static void main(String args[]) {%2$c%1$c%1$c// Outside%2$c%1$c%1$cString src = %3$c%4$s%3$c;%2$c%1$c%1$cSystem.out.printf(src, 9, 10, 34, src);%2$c%1$c%1$croutine();%2$c%1$c}%2$c}%2$c";
 		System.out.printf(src, 9, 10, 34, src);
 		routine();
 	}
---
+++ Java/Grace.java
@@ -0,0 +1,8 @@
+public class Grace {
+    public static void main(String args[]) {
+        public final static String KID = "Grace_kid.java";
+        public final static String SOURCE = "public class Grace{%2$c%"
+
+        System.out.printf(SOURCE, 9, 10, 34, SOURCE)
+    }
+}
